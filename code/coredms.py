e066446e9ed206c2d83a683f0d443b23c7a694ad
yagublj1 <jeyhun.yagublu@aalto.fi>
2022-02-16 21:17:21 +0200

    designed and implemented coredms and data ingest , maybe need some changes

---
+++ code/coredms.py
@@ -0,0 +1,56 @@
+import datetime
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
+import sys
+from time import time
+KEYSPACE="mysimbdp"
+
+if __name__ == "__main__":
+   
+    if (sys.argv[1])=="setup_cassandra":
+   
+        for i in range(0,3):
+            cluster = Cluster(['0.0.0.0'],port=9042+i)
+            session = cluster.connect()
+            print("i am working")
+            #cluster = Cluster(['0.0.0.0'], port=9042)
+
+            session.execute("""
+                        CREATE KEYSPACE IF NOT EXISTS mysimbdp
+                        WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '3' }
+                        """ )
+            session.set_keyspace("mysimbdp")
+
+            session.execute(""" DROP TABLE IF EXISTS listings """)
+
+            session.execute("""
+                        CREATE TABLE IF NOT EXISTS listings (
+                            id int,
+                            host_id int,
+                            host_name text,
+                            neighbourhood text,
+                            latitude float,
+                            longitude float, 
+                            room_type text,
+                            price int,
+                            availability_365 int,
+                            PRIMARY KEY (id, host_id)
+                        )
+                        """)
+            print("created a set_keyspace")
+
+    elif (sys.argv[1]=="get_listings"):
+        
+        
+        cluster = Cluster(['0.0.0.0'],port=9042)
+        session = cluster.connect("mysimbdp", wait_for_all_pools=True)
+        session.execute('USE %s' % KEYSPACE)
+        start=time()
+        rows = session.execute('SELECT * FROM listings')
+        end=time()
+        
+       
+        #print(list(rows))
+        print("Got Listings",len(list(rows))  ,"in", end-start, "s", sep=" ")
+            
---
+++ code/dataingest.py
@@ -0,0 +1,80 @@
+import os
+import csv
+import sys
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement
+
+from time import time
+import datetime
+import argparse
+
+def ingestListings(datafile, session, ):
+    #print("Starting batch load if listings entries...")
+    query = session.prepare('INSERT INTO mysimbdp.listings (id,  host_id , host_name,  neighbourhood ,latitude ,longitude, room_type , price , availability_365 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)')
+
+
+    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+    #insertions = 0
+    with open(datafile) as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        next(csv_file)
+
+        insertions=0
+        for row in reader:
+            
+            try:
+                id=int(row[0])
+                host_id=int(row[1])
+                host_name=row[2]
+                neighbourhood=row[3]
+                latitude=float(row[4])
+                longitude=float(row[5])
+                room_type=row[6]
+                price=int(row[7])
+                availability_365=int(row[8])
+                batch.add(query,
+                 (id,  host_id , host_name,  neighbourhood ,latitude ,longitude,
+                  room_type , price , availability_365 )
+                #   (int(row[0]),int(row[1]),row[2], row[3],
+                #         float(row[4]),float(row[5]),row[6],
+                #         int(row[7]),int(row[8]))
+                  )
+                
+            except Exception as e:
+                print('The cassandra error: {}'.format(e))
+
+           
+           
+           
+            
+            try:
+                session.execute(batch)
+                batch.clear()
+                #insertions += 1
+                insertions+=1
+                #print("Success")
+                if(insertions==200000):
+                    break 
+                
+            except Exception as e:
+                print('The cassandra error: {}'.format(e))
+
+           
+    return insertions
+
+
+if __name__ == "__main__":
+    cluster = Cluster(['0.0.0.0'],port=9042)
+    session = cluster.connect()
+
+    table = sys.argv[1]
+    datafile = sys.argv[2]
+
+    start=time()
+    if table == "listings":
+	    insertions = ingestListings(datafile, session)
+
+    stop=time()
+
+    print("Inserted", insertions, "in", stop-start, "s", sep=" ")
