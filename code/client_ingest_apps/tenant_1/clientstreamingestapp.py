ae92a77fafa31bba7dc7be93392800dc83d25941
yagublj1 <jeyhun.yagublu@aalto.fi>
2022-03-17 03:21:16 +0200

    Producer done

---
+++ code/client_ingest_apps/tenant_1/clientstreamingestapp.py
@@ -0,0 +1,63 @@
+import os
+from datetime import datetime
+import sys
+UTILS_PATH = os.getcwd()
+print(os.getcwd())
+sys.path.append(UTILS_PATH)
+import csv
+import requests
+import logging
+import pandas as pd
+from cassandra.query import BatchStatement
+from cassandra import ConsistencyLevel
+from mysimbdp import MySimBdp_CommonTool
+
+
+
+
+
+    
+       
+class ClienStreamIngestApp():
+    
+    def stream_ingest(self, table_name, data):
+        configuration = {
+            "tenant_id": "tenant_1",
+            "tables": [
+                    {
+                        "table_name": "listings",
+                        "primary_key": ["host_id", "id"],
+                        "schema": [
+                            {"field": "id", "type": "int"},
+                            {"field": "host_id", "type": "int"},
+                            {"field": "host_name", "type": "text"},
+                            {"field": "neighbourhood", "type": "text"},
+                            {"field": "latitude", "type": "float"},
+                            {"field": "longitude", "type": "float"},
+                            {"field": "room_type", "type": "text"},
+                            {"field": "price", "type": "int"},
+                            {"field": "availability_365", "type": "int"}
+                        ]
+                    },
+                    {
+                        "table_name": "Listing_table_2",
+                        "primary_key": ["host_id", "id"],
+                        "schema": [
+                            {"field": "id", "type": "int"},
+                            {"field": "host_id", "type": "int"},
+                            {"field": "host_name", "type": "text"},
+                            {"field": "neighbourhood", "type": "text"},
+                            {"field": "latitude", "type": "float"},
+                            {"field": "longitude", "type": "float"},
+                            {"field": "room_type", "type": "text"},
+                            {"field": "price", "type": "int"},
+                            {"field": "availability_365", "type": "int"}
+                        ]
+                    }
+                ]
+            }
+
+                
+        print ("Table and data are" + table_name ,data)
+
+   
---
+++ code/mysimbdp/streamingestmanager.py
@@ -0,0 +1,80 @@
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler
+import json
+import logging
+import pika, sys, os, time, requests
+import threading
+import json
+import importlib
+
+RB_HOST = "35.228.109.23"
+CRENDENTIALS = pika.PlainCredentials('mysimbdp', 'mysimbdp')
+CURRENT_DIRECTORY = os.getcwd()
+
+
+
+class ConsumerThread(threading.Thread):
+
+    def __init__(self,  tenant, *args, **kwargs):
+        super(ConsumerThread, self).__init__(*args, **kwargs)
+
+        self._host = '0.0.0.0'
+        self._tenant = tenant
+
+    def run(self):
+       
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=self._host, virtual_host=self._tenant['tenant_id'], credentials=CRENDENTIALS))
+        channel = connection.channel()
+
+        # declare exchange
+        channel.exchange_declare(exchange='default', exchange_type='direct')
+
+        # declare queues and bind it to exchange (one table has one queue)
+        for table in self._tenant['tables']:
+            # declare queue
+            result = channel.queue_declare(queue='', exclusive=True)
+            queue_name = result.method.queue
+            # bind queue to exchange
+            channel.queue_bind(exchange='default', queue=queue_name, routing_key=table)
+            # define what to do when accept data from rabbitmq
+            def callback(ch, method, properties, body):
+                print(" [mysimbdp-streamingestmanager] <Tenant={} Table={}> Received msg".format(self._tenant['tenant_id'], table, body.decode()))
+                current_directory = os.getcwd()
+                # invoke customer's clientstreamingestapp
+
+                #os.system("python3 {current}/../client_ingest_apps/{tenant_id}/clientstreamingestapp.py {table_name} {file_name}".format(current=CURRENT_DIRECTORY, tenant_id=self._tenant['tenant_id'], table_name=table, file_name=body.decode()))
+                print("Trying to upload module")
+                
+                clientstreamingestapp = importlib.import_module("..client_ingest_apps.{}.clientstreamingestapp.py".format(self._tenant['tenant_id']))
+                clientstreamingestapp.stream_ingest(table,body.decode())
+     
+                 # ack the message
+                ch.basic_ack(delivery_tag = method.delivery_tag)
+            # consume the queue
+            channel.basic_consume(queue=queue_name, on_message_callback=callback)
+
+        print('[mysimbdp-streamingestmanager] Start consuming vhost: {}'.format(self._tenant['tenant_id']))
+        channel.start_consuming()
+
+
+def main():
+
+    tenants = [
+        {
+            'tenant_id': 'tenant_1',
+            'tables': ['listing']
+         }#,
+        # {
+        #     'tenant_id': 'tenant_2',
+        #     'tables': ['another_listing']
+        # }
+    ]
+    threads = [ConsumerThread( tenant=tenant) for tenant in tenants]
+    for thread in threads:
+        thread.start()
+
+
+if __name__ == '__main__':
+    main()
